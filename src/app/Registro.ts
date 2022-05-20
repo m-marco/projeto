e1a63f68fcb0856e748445cf36b2887fef7be5e3
Marco Antônio <marco1002@live.com>
2022-05-20 18:08:39 -0300

    iniciando

---
+++ src/app/Registro.ts
@@ -0,0 +1,6 @@
+export class Registro {
+  Nome: string = '';
+  RU: number = 0;
+  Curso: string = '';
+  DataAniversario: string = '';
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { Registro } from './Registro';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'projeto';
+  registros: Registro[] = [];
+  onSubmit(f: NgForm) {
+    let dados = f.value;
+    let newRegistro = new Registro();
+    newRegistro.Curso = dados.curso;
+    newRegistro.DataAniversario = dados.aniversario;
+    newRegistro.Nome = dados.nome;
+    newRegistro.RU = dados.ru;
+    this.registros.push(newRegistro);
+    f.reset();
+  }
 }
